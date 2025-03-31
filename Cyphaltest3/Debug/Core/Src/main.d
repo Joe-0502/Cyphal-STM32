2b24700de7bd750b904470b7cfe4b37b7abf6fcb
Joe-0502 <a31526409@gmail.com>
2025-03-31 12:20:40 +0800

    1

---
+++ Cyphaltest3/Debug/Core/Src/main.d
@@ -0,0 +1,70 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ ../Core/Inc/stm32l4xx_hal_conf.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_can.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/canard.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/canard.c \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/canard.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/cavl.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/node/Heartbeat_1_0.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/nunavut/support/serialization.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/node/Health_1_0.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/node/Mode_1_0.h \
+ C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/primitive/array/Real64_1_0.h
+../Core/Inc/main.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+../Core/Inc/stm32l4xx_hal_conf.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_can.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/canard.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/canard.c:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/canard.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/libcanard/cavl.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/node/Heartbeat_1_0.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/nunavut/support/serialization.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/node/Health_1_0.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/node/Mode_1_0.h:
+C:/Users/joe/Desktop/Github/Cyphal_STM32/Cyphaltest3/uavcan/primitive/array/Real64_1_0.h:
